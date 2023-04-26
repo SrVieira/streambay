8c521a182d01dcae23bbb0a6f171bb1fc593ecaa
SrVieira <matheusvieirasto@gmail.com>
2023-04-26 11:47:29 -0300

    create componetn slider

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -3,11 +3,14 @@ import React from 'react';
 // COMPONENTS
 import { Header } from '@components/Header';
 import { Footer } from '@components/Footer';
+import { Slider } from '@components/Slider';
 
 const Layout = ({ children }): JSX.Element => {
     return (
         <div >
-            <Header />
+            <Slider>
+                <Header />
+            </Slider>
             {children}
             <Footer />
         </div>
---
+++ components/Slider/Slider.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Slider = ({ children }): JSX.Element => {
+    return (
+        <div>
+            {children}
+            slider
+        </div>
+    );
+};
+
+export default Slider;
